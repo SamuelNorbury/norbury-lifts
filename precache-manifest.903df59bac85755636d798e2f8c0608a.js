66c8069758867fcc5e603dc63a235285cc3014d7
SamuelNorbury <samuel@studocu.com>
2020-02-22 23:00:38 +0000

    Deploying to gh-pages from  - 9812ecd65578f211d890a16ef374ed3131e3bd60 🚀

---
+++ precache-manifest.903df59bac85755636d798e2f8c0608a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "701428407c099e6b802d",
+    "url": "/static/css/main.1d9b7957.chunk.css"
+  },
+  {
+    "revision": "701428407c099e6b802d",
+    "url": "/static/js/main.3f2aa36d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "179efbbfaffdd06b71f6",
+    "url": "/static/js/2.f66289ba.chunk.js"
+  },
+  {
+    "revision": "c41e8719ef6182d309eae1e6f975b6aa",
+    "url": "/index.html"
+  }
+];
